a1c11b7bc4acfcf1c53da5dd204e8fbc0fbc4fb9
Jeffrey Blayney <jeffreyblayney@Jeffreys-MacBook-Pro.local>
2018-10-14 19:53:36 -0600

    Including venv setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 brew install mpg321
 brew install portaudio
+virtualenv venv -p /usr/local/bin/python3.6
 source venv/bin/activate
 pip3 install -r requirements.txt
+deactivate
+echo "activate with: source venv/bin/activate"
