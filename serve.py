64c9b17641c3eb7eed1a91ee471cc4152cd80c53
Jeffrey Blayney <jeffreyblayney@Jeffreys-MacBook-Pro.local>
2018-10-14 19:27:03 -0600

    alfred v2

---
+++ serve.py
@@ -0,0 +1,30 @@
+import sys
+sys.path.insert(0, '/lib')
+from lib import listen, speak, question
+
+speaker = speak.Speak()
+listener = listen.Listen()
+questioner = question.Question()
+
+# speaker.speak('Good Morning, I am Alfred, how can I help you today?')
+
+speaker.speak('Say something biatch')
+
+received_question = False
+
+while received_question is False:
+    response = listener.listen()
+
+    if questioner.is_a_question(response):
+        # TODO: Check if I've heard this question before
+        speaker.speak('Good question, I think you said ' + response + ', is that correct?')
+    else:
+        speaker.speak('I think you said ' + response + ', is that correct?')
+
+    response = listener.listen()
+
+    if questioner.is_yes_response(response):
+        speaker.speak('Awesome. I\'m still pretty dumb, and I don\'t know the answer to your question')
+        received_question = True
+    elif questioner.is_no_response(response):
+        speaker.speak('I\'m sorry about that, can you repeat your question?')
